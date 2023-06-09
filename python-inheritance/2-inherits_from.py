06bd806acc268bdee23025d2d229c2e0e6291511
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-09 14:09:32 -0700

    Part two

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,5 +6,5 @@
 
 
 def inherits_from(obj, a_class):
-    """ returns true if the class is a subclass the """
-    return issubclass(obj, a_class)
+    """ returns true if the object inhetirts from the specified class """
+    return issubclass(obj, a_class) and (type(obj) != a_class)
