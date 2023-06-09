38314ed318dd103af67641a010f18f1fe453dea1
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-09 14:48:48 -0700

    Part four

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+   class based on the base geometry class with a public instance 
+   method that raises an exception.
+"""
+
+
+class BaseGeometry:
+    """ Basing the class on the empty BaseGeometry """
+
+    def area(self):
+        """ Public instance method of the area, raises excpetion 
+        if area isn't use. """
+        raise Exception("area() is not implemented")
