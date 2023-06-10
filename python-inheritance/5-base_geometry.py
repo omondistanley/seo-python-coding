e6a2f41fe6a8ee8c38d18c591fc7b43de5a51d3c
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-09 18:45:27 -0700

    Part five

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Base Geometry class based on 4-base_geometry class. Has the area method
+    which gives an exception if the area methos isn't used. The class also has
+    another public instance method that validates integers.
+"""
+
+
+class BaseGeometry:
+    """ Based on the empty base geometry class and 4-base_geometry. """
+
+    def area(self):
+        """ Public instance method  of the area, raises exception if the area
+        isn't used. """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """ Public instance method that validates the value """
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
