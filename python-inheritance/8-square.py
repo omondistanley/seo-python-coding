bbb469a6070fe8807e92120f541696d37d5353bb
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-28 08:23:05 -0700

    Part eight

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+    docstring documentation
     A square class that inherits from the 7-rectangle with
     instantiation that initializes the size, which must be
     private and a positive integer. The area method must be
@@ -14,12 +15,16 @@ class Square:
     """ The square class inheriting from rectangle class. """
 
     def __init__(self, size):
+        """ The method initializes the size to be a positive int
+            and set to be a private variable. """
         super().Integer_validator("size", size)
         self._size = size
 
     def area(self):
+        """ Calculates and returns the area of the square. """
         return self._size * self._size
 
     def __str__(self):
+        """ Returns the size of the square and name rectangle. """
         width = str(self._size)
         return "[Rectangle] " + width + "/" + width 
